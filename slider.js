581ee50d82610dc052fb03d61ba3285c2a804220
ShipaShipovnik <shipovnik2020d@gmail.com>
2023-10-04 20:23:15 +0300

    commit

--- slider.js
+++
@@ -1,7 +0,0 @@
-let offset = 0; //смещение от левого края
-const sliderline = document.querySelector('.slider-line');
-
-document.querySelector('.slider-next').addEventListener('click',function(){
-    offset = offset+1154;
-    sliderline.style.left = offset+'px';
-});
